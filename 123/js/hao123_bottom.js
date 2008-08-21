f74af8e60fc9ea4fcf314422f2813683bfaae7ab
yongjin <yongjin@354dec82-773a-0f43-a7ef-e1013d8ebae5>
2008-08-21 08:37:19 +0000

    关于我们修改,底部js链接路径修改
    git-svn-id: https://www.xdanger.com/svn/verycd2/very.cd/trunk@12119 354dec82-773a-0f43-a7ef-e1013d8ebae5

--- 123/js/hao123_bottom.js
+++ 123/js/hao123_bottom.js
@@ -1,4 +1,4 @@
 document.writeln("<table width=778  border=0 cellspacing=0 cellpadding=0>");
 document.writeln("<tr>");
-document.writeln("<td align=center><a href=../abouthao123.htm >关于我们</a>--<a href=../hao123home/help.htm>本站帮助</a>--<a href=../hao123map.htm>网站地图</a>--<a href=http://zhidao.baidu.com/browse/1039>帮你找站</a>--<a href=../gongyihd/index.htm >公益宣传</a>--<a href=http://tieba.baidu.com/f?kw=7999>网站提交</a>");
+document.writeln("<td align=center><a href=../../123/abouthao123.htm >关于我们</a>--<a href=../../123/hao123home/help.htm>本站帮助</a>--<a href=../../123/hao123map.htm>网站地图</a>--<a href=http://zhidao.baidu.com/browse/1039>帮你找站</a>--<a href=../../123/gongyihd/index.htm >公益宣传</a>--<a href=http://tieba.baidu.com/f?kw=7999>网站提交</a>");
 document.writeln("</td></tr></table>");
