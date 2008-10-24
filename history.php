b8471279a4e5dbba5ffe4473c350e6864c365275
xdanger <xdanger@354dec82-773a-0f43-a7ef-e1013d8ebae5>
2008-10-24 04:18:27 +0000

    debug：session.gc_maxlifetime有问题，不能设
    git-svn-id: https://www.xdanger.com/svn/verycd2/very.cd/trunk@13488 354dec82-773a-0f43-a7ef-e1013d8ebae5

--- history.php
+++ history.php
@@ -9,7 +9,7 @@ include_once './api/functions.inc.php';
 $expires = 3600 * 24 * 365;
 ini_set('session.save_handler', 'memcache');
 ini_set('session.save_path', 'tcp://localhost:11211?persistent=0&weight=1&timeout=1&retry_interval=3');
-ini_set('session.gc_maxlifetime', $expires);
+//ini_set('session.gc_maxlifetime', $expires);
 session_set_cookie_params($expires, '/');
 session_cache_limiter('private');
 session_cache_expire($expires / 60);
@@ -21,11 +21,11 @@ if (!array_key_exists('h', $_SESSION) || !is_array($_SESSION['h'])) {
 	$_SESSION['h'] = array();
 }
 
-if (array_key_exists('action', $_POST) && $_POST['action'] == 'empty') {
+if (array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'empty') {
 	$_SESSION['h'] = array();
-} elseif (array_key_exists('url', $_POST) && array_key_exists('txt', $_POST)) {
-	$url = urldecode($_POST['url']);
-	$txt = urldecode($_POST['txt']);
+} elseif (array_key_exists('url', $_REQUEST) && array_key_exists('txt', $_REQUEST)) {
+	$url = urldecode($_REQUEST['url']);
+	$txt = urldecode($_REQUEST['txt']);
 	$ign = false;
 	foreach ($ignore_preg as $preg) {
 		if (preg_match($preg, $url)) {
