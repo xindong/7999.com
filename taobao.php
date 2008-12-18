bffe6c1ebe081d93fb21bd99dd09bd12db5649f1
xdanger <xdanger@354dec82-773a-0f43-a7ef-e1013d8ebae5>
2008-12-18 03:00:34 +0000

    淘宝搜索换成带广告的
    git-svn-id: https://www.xdanger.com/svn/verycd2/very.cd/trunk@15341 354dec82-773a-0f43-a7ef-e1013d8ebae5

---
+++ taobao.php
@@ -0,0 +1,3 @@
+<?php
+
+header('Location: http://adtaobao.allyes.cn/main/adfclick?db=adtaobao&bid=2400,2351,636&cid=32312,469,1&sid=65146&show=ignore&url=http://search1.taobao.com/browse/search_auction.htm?commend=all&sort=&channel=prefill&ie=gb&oe=UTF-8&hl=zh-CN&q='.urlencode($_GET['q']));
